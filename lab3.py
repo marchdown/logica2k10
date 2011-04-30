1cbbfe3e6821ac803d1bfc5d7fb94641f00a605e
marchdown <marchdown@gmail.com>
2011-04-30 22:20:00 +0100

    Вычистил лишние вспомогательные функции и волшебные значения

--- lab3.py
+++ lab3.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# module lab3
-import sys, re
-import logging 
-#DEBUG = True
+import sys, re, logging 
+log = logging.getLogger("lab3")
+log.setLevel(logging.DEBUG)
 
-
-Expansion_Rules_raw = """
-E->E+T | T
-T->T*F | F
-F->(E) | a
-"""
 ########################################
-####  В этом блоке разбирается текстовое представнение таблиц предшествования и правил вывода
+####  В этом блоке задаются вспомогательные функции
+####  для приведения таблиц предшествования 
+####  и правил вывода в пригодный для работы вид
+
 def parse_grammar(G, reL):
   rules = [parse_rule(r, reL) for r in G[1:-1].split('\n')]
   return expand_all_rules(rules)
@@ -32,7 +28,41 @@ def expand_rule(r):
   for e in expansion_results:
     res.append((expansion_base, e))
   return res
+######################################## Дальше разбрается таблица
+
+def Alphabet_And_Dict_From_Precedence_Table(Precedence_Table_raw):
+  Precedence_Table_lines = Precedence_Table_raw.split('\n')[3:] # Список строк таблицы предшествования
+  L = list(l[0:1] for l in Precedence_Table_lines)         # Список символов алфавита
+  reL ='['+''.join(L)+']'              # в форме, годной для регулярного выражения
+  hs = enumerate(L)                    # Нумерованный алфавит в форме итератора
+  ls = list(l[3:]+' ' for l in Precedence_Table_lines)   # Строки с отрезанными головами
+  return reL, dict_from_enumerated_heads_and_lines(hs, ls)
+
+def dict_from_enumerated_heads_and_lines(heads,ls):
+  hs = list(heads)
+  log.debug("heads are %s", hs)
+  d = {}
+#  a = []
+  for x, h in hs:
+    log.debug("outer loop x:%s, h:%s", x, h)
+    d[h] = {}
+#    a[x] = []
+    for y, g in hs:
+      log.debug("inner loop y:%s, g:%s", y, g)
+      d[h][g] = ls[x][3*y:3*(y+1)]
+#      a[x][y] = ls[x][3*y:3*(y+1)]
+  return d
+
 ########################################
+#### Куски текста, которые надо разобрать
+
+#### Третья лаба
+# Правила преобразования
+Expansion_Rules_raw = """
+E->E+T | T
+T->T*F | F
+F->(E) | a
+"""
 # Таблица предшествования
 Precedence_Table_raw = '''
   | E  T  F  a  (  )  +  *  #
@@ -46,61 +76,18 @@ a |                >  >  >  >
 + |    <= <  <  <            
 * |       =  <  <            
 # | <  <  <  <  <            '''
-
-def Alphabet_From_Precedence_Table(Precedence_Table_raw):
-  Precedence_Table_lines = Precedence_Table_raw.split('\n')[3:]
-  # Список строк таблицы предшествования
-  L = list(l[0:1] for l in Precedence_Table_lines)         # Список символов алфавита
-  reL ='['+''.join(L)+']'              # в форме, годной для регулярного выражения
-  return reL
-def Alphabet_And_Dict_From_Precedence_Table(Precedence_Table_raw):
-  Precedence_Table_lines = Precedence_Table_raw.split('\n')[3:]
-  # Список строк таблицы предшествования
-  L = list(l[0:1] for l in Precedence_Table_lines)         # Список символов алфавита
-  reL ='['+''.join(L)+']'              # в форме, годной для регулярного выражения
-  hs = enumerate(L)                    # Нумерованный алфавит
-  ls = list(l[3:]+' ' for l in Precedence_Table_lines)   # Строки с отрезанными головами
-  print hs
-  return reL, dict_from_enumerated_heads_and_lines(hs, ls)
-
-# [индексы] * [индексы] * [строки] -> {индекс}{индекс}{ячейка}
-def dict_from_enumerated_heads_and_lines(heads,ls):
-  hs = list(heads)
-  logging.debug("heads are %s", hs)
-  d = {}
-#  a = []
-  for x, h in hs:
-    logging.debug("outer loop x:%s, h:%s", x, h)
-    d[h] = {}
-#    a[x] = []
-    for y, g in hs:
-      logging.debug("inner loop y:%s, g:%s", y, g)
-      d[h][g] = ls[x][3*y:3*(y+1)]
-#      a[x][y] = ls[x][3*y:3*(y+1)]
-  return d
-  
-def parsePrecTable(G):
-  lines = G('\n')[3:]
-  L = list(l[0:1] for l in lines)
-  return (L, PrecTuples)
-#G8 = ((s, r, G6[i][3*j:3*(j+1)]) for i, s in Ln for j, r in Ln)     # Кортежи вида (символ, символ, значение предшествования)
-#G9 = [(L[i], L[j], Precedence_Table_data[i][3*j:3*(j+1)]) for i in range(len(L)) for j in range(len(L))]
-#Gdict = {(s,r):G6[i][3*j:3*(j+1)] for i, s in Ln for j, r in Ln}
-
-def T(x, y):
-  for i in range(len(G9)):
-    if G9[i*9][0] == x:
-      for j, r in enumerate(L):
-        if y == G9[i*9 + j][1]:
-          return G9[i*9 + j][2]
-
-G = parse_grammar(Expansion_Rules_raw, Alphabet_From_Precedence_Table(Precedence_Table_raw))
-#G = [("E", "E+T"),("E", "T"),("T", "T*F"),("T", "F"),("F", "(E)"),("F", "a")]
-
+#### Надо заметить, что пока я не вывожу N, T, S из правил, а выписываю вручную. 
 T = 'a()+*'
 N = 'ETF'
-R = parse_grammar(Expansion_Rules_raw, '['+T+N+']')
 S = '#'
+#### Что получается после разбора и используется в программе:
+R = parse_grammar(Expansion_Rules_raw, Alphabet_From_Precedence_Table(Precedence_Table_raw))
+#R = parse_grammar(Expansion_Rules_raw, '['+T+N+']')
+#R = [("E", "E+T"),("E", "T"),("T", "T*F"),("T", "F"),("F", "(E)"),("F", "a")]
+reL, PrecTable = Alphabet_And_Dict_From_Precedence_Table(Precedence_Table_raw)
+
+def Prec(h, g):          #G(T) из задания. Имя T уже занято под множество конечных символов
+  return PrecTable[h][g]
 ########################################
 #### Основная логика программы
 #    Программа принимает на вход выражение и проверяет его на корректность
@@ -110,17 +97,17 @@ S = '#'
 #
 #
 def validate(s_raw):
-  logging.debug( "проверяю выражение %s", s_raw)
+  log.debug( "проверяю выражение %s", s_raw)
   m = "#"
   s = s_raw+'#'
   for i in range(len(s)):
     x = m[-1]
     y = s[i]
     t = T(x,y)
-    logging.debug( 'сравниваю: %s и %s -- %s', x, y, t)
+    log.debug( 'сравниваю: %s и %s -- %s', x, y, t)
     if '<' in t or '=' in t:
       m += y
-      logging.debug( 'в таблице есть <, наращиваю стек: %s+%s', m[:-1], m[-1])
+      log.debug( 'в таблице есть <, наращиваю стек: %s+%s', m[:-1], m[-1])
     if '>' in t:
       m = '#'+ recursive_sverni(m[1:]) # there are no procedures in python
       # FIXME: check if m is changed 
@@ -129,7 +116,7 @@ def validate(s_raw):
       # break from cycle
   #end cycle
     if t == '   ':
-      logging.debug( 'По таблице предшествования', x, 'никогда не встечается перед',y, 'в правильном выражении')
+      log.debug( 'По таблице предшествования', x, 'никогда не встечается перед',y, 'в правильном выражении')
 #      if FROM_TEST: print '\n', 'несмотря на то, что это выражение было порождено с помощью тех же правил подстановки, что используются при свертке\n', 'приходится счесть его некорректным'
       break
   return m == "#E"
@@ -139,9 +126,9 @@ def validate(s_raw):
 def recursive_sverni(s):
   res = sverni(s)
   if res == s:
-    logging.debug( 'при последнем прогоне ничего не изменилось, хватит сворачивать')
+    log.debug( 'при последнем прогоне ничего не изменилось, хватит сворачивать')
     return res # break
-  logging.debug( s, '->', res, 'стек изменился, надо сворачивать заново')
+  log.debug( s, '->', res, 'стек изменился, надо сворачивать заново')
   res = recursive_sverni(res)
   return res
 
@@ -149,15 +136,15 @@ def recursive_sverni(s):
 def sverni(s):
   res = s
   for i in range(len(res)):
-    logging.debug( 'сворачиваю', s[-i:])
+    log.debug( 'сворачиваю', s[-i:])
     for rule in G:
-      logging.debug( 'пробую правило', rule)
+      log.debug( 'пробую правило', rule)
       if rule[1] == s[-i:]:  # check bounds
-        logging.debug( 'заменяю',s[-i:],'на',rule[0])
+        log.debug( 'заменяю',s[-i:],'на',rule[0])
         res = s[:-i] + rule[0]
         break #return res
     if res != s: 
-      logging.debug( 'готово дело')
+      log.debug( 'готово дело')
       break
 
   return res
@@ -176,11 +163,11 @@ def sravni(x,y):
 ################################################################################
 #	Дальше идут проверки
 # К счастью, для проверки достаточно породить несколько выражений
-def expand(expr, rules=G, depth=3, exprs = []):
+def expand(expr, rules=R, depth=3, exprs = []):
 #  global.FROM_TEST = True
   if depth == 0:
     return exprs
-  logging.debug( 'depth', depth)
+  log.debug( 'depth', depth)
   depth -= 1
 #  res = exprs.append(expr)
   if not exprs:
@@ -189,19 +176,18 @@ def expand(expr, rules=G, depth=3, exprs = []):
     res = exprs
   new_res = []
   for e in res:
-    logging.debug( 'processing expression', e)
+    log.debug( 'processing expression', e)
     for r in rules:
-      logging.debug( 'trying rule', r, 'on expression', e, 'of', res)
+      log.debug( 'trying rule', r, 'on expression', e, 'of', res)
       
       pos = e.find(r[0])
       if pos+1: 
-        logging.debug( 'augmenting', e, 'with', r)
+        log.debug( 'augmenting', e, 'with', r)
         augment = e[:pos] + r[1] + e[pos+len(r[1]):]
         if augment not in res:
           new_res.append(augment)
 #        res = list(set(res))
   return expand(expr, depth=depth, exprs=new_res)
-      
     
 def main():
   return 0
